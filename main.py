d4513f12b18f98b1f06aecc58e9ef7f5debff685
garryaklia31 <158801619+garryaklia31@users.noreply.github.com>
2024-04-21 21:51:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -45,12 +45,12 @@ async def start(m: UpdateNewMessage):
     reply_text = f"""
 Hello! I am a bot to download videos from terabox.
 Send me the terabox link and I will start downloading it.
-Join @Ultroid_Official For Updates
-[Source Code](https://github.com/sahildesai07/terabox-downloader-bot) """
-    check_if = await is_user_on_chat(bot, "@Ultroid_Official", m.peer_id)
+Join @TERABOXSUPPORT01 For Updates
+"""
+    check_if = await is_user_on_chat(bot, "@TERABOXSUPPORT01", m.peer_id)
     if not check_if:
-        return await m.reply("Please join @Ultroid_Official then send me the link again.")
-    check_if = await is_user_on_chat(bot, "@UltroidOfficial_chat", m.peer_id)
+        return await m.reply("Please join @TERABOXSUPPORT01 then send me the link again.")
+    check_if = await is_user_on_chat(bot, "@TERABOXSUPPORT01", m.peer_id)
     if not check_if:
         return await m.reply(
             "Please join @UltroidOfficial_chat then send me the link again."
@@ -69,13 +69,13 @@ Join @Ultroid_Official For Updates
 async def start(m: UpdateNewMessage):
     text = m.pattern_match.group(1)
     fileid = db.get(str(text))
-    check_if = await is_user_on_chat(bot, "@Ultroid_Official", m.peer_id)
+    check_if = await is_user_on_chat(bot, "@TERABOXSUPPORT01", m.peer_id)
     if not check_if:
-        return await m.reply("Please join @Ultroid_Official then send me the link again.")
-    check_if = await is_user_on_chat(bot, "@UltroidOfficial_chat", m.peer_id)
+        return await m.reply("Please join @TERABOXSUPPORT01 then send me the link again.")
+    check_if = await is_user_on_chat(bot, "@TERABOXSUPPORT01", m.peer_id)
     if not check_if:
         return await m.reply(
-            "Please join @UltroidOfficial_chat then send me the link again."
+            "Please join @TERABOXSUPPORT01 then send me the link again."
         )
     await bot(
         ForwardMessagesRequest(
@@ -126,16 +126,16 @@ async def handle_message(m: Message):
     url = get_urls_from_string(m.text)
     if not url:
         return await m.reply("Please enter a valid url.")
-    check_if = await is_user_on_chat(bot, "@Ultroid_Official", m.peer_id)
+    check_if = await is_user_on_chat(bot, "@TERABOXSUPPORT01", m.peer_id)
     if not check_if:
-        return await m.reply("Please join @Ultroid_Official then send me the link again.")
-    check_if = await is_user_on_chat(bot, "@UltroidOfficial_chat", m.peer_id)
+        return await m.reply("Please join @TERABOXSUPPORT01 then send me the link again.")
+    check_if = await is_user_on_chat(bot, "@TERABOXSUPPORT01", m.peer_id)
     if not check_if:
         return await m.reply(
-            "Please join @UltroidOfficial_chat then send me the link again."
+            "Please join @TERABOXSUPPORT01 then send me the link again."
         )
     is_spam = db.get(m.sender_id)
-    if is_spam and m.sender_id not in [6695586027]:
+    if is_spam and m.sender_id not in [1718738592]:
         return await m.reply("You are spamming. Please wait a 1 minute and try again.")
     hm = await m.reply("Sending you the media wait...")
     count = db.get(f"check_{m.sender_id}")
@@ -187,7 +187,7 @@ async def handle_message(m: Message):
         return await hm.edit(
             f"Sorry! File is not supported for now. I can download only .mp4, .mkv and .webm files."
         )
-    if int(data["sizebytes"]) > 524288000 and m.sender_id not in [6695586027]:
+    if int(data["sizebytes"]) > 1718738592 and m.sender_id not in [1718738592]:
         return await hm.edit(
             f"Sorry! File is too big. I can download only 500MB and this file is of {data['size']} ."
         )
@@ -237,7 +237,7 @@ async def handle_message(m: Message):
             caption=f"""
 File Name: `{data['file_name']}`
 Size: **{data["size"]}** 
-Direct Link: [Click Here](https://t.me/TeraboxDownloadeRobot?start={uuid})
+Direct Link: [Click Here](https://t.me/TeraboxDownloadeR_Robot?start={uuid})
 
 @Ultroid_Official
 """,
@@ -261,7 +261,7 @@ Direct Link: [Click Here](https://t.me/TeraboxDownloadeRobot?start={uuid})
             caption=f"""
 File Name: `{data['file_name']}`
 Size: **{data["size"]}** 
-Direct Link: [Click Here](https://t.me/TeraboxDownloadeRobot?start={uuid})
+Direct Link: [Click Here](https://t.me/TeraboxDownloader_Robot?start={uuid})
 
 @ultroid_official
 """,
